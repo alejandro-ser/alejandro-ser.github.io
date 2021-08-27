c9f8c0c2774440425e9ab462a927b09219e33e02
Alejo.Ser <alejosegi@gmail.com>
2021-08-27 11:20:09 +0200

    Scroll section active, cv photo

--- new/js/script.js
+++ new/js/script.js
@@ -111,3 +111,26 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp)
+
+// ==================== SCROLL SECTION ACTIVE HIGHLIGHT ====================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+    console.log("Section highlight is working!")
+}
+
+window.addEventListener('scroll', scrollActive)
